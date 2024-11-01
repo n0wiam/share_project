ea9c293373032b6e215d8f4949ab02c278145cd8
nowiam <3159412294@qq.com>
2024-11-01 16:45:27 +0800

    朋友关系缓存

--- src/main/java/com/nowiam/service/impl/FriendServiceImpl.java
+++ src/main/java/com/nowiam/service/impl/FriendServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nowiam.service.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.nowiam.mapper.FriendMapper;
 import com.nowiam.mapper.UserMapper;
 import com.nowiam.model.Result;
@@ -24,23 +26,39 @@ public class FriendServiceImpl implements FriendService {
     UserMapper userMapper;
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    Gson gson;
 
     private static final String KEY="FRIEND_LIST:";
+    private static final String REQ="REQ:";
+    private static final String ACC="ACC:";
 
     @Override
     public Result myFriend() {
         Integer userId= ThreadLocalUtil.getUser().getId();
+        //获取缓存
+        List<User> cache=getCache(KEY+ACC+userId);
+        if(cache!=null) return new Result<>().ok(cache);
+        //读取数据库
         List<Integer> list=friendMapper.myFriend(userId);
+        List<User> res=getFriend(list);
         //缓存
-        return new Result<>().ok(getFriend(list));
+        putCache(res,KEY+ACC+userId);
+        return new Result<>().ok(res);
     }
 
     @Override
     public Result friendReq() {
         Integer userId= ThreadLocalUtil.getUser().getId();
+        //获取缓存
+        List<User> cache=getCache(KEY+REQ+userId);
+        if(cache!=null) return new Result<>().ok(cache);
+
         List<Integer> list=friendMapper.friendReq(userId);
+        List<User> res=getFriend(list);
         //缓存
-        return new Result<>().ok(getFriend(list));
+        putCache(res,KEY+REQ+userId);
+        return new Result<>().ok(res);
     }
 
     @Override
@@ -63,8 +81,8 @@ public class FriendServiceImpl implements FriendService {
         friendCon.setStatus(0);
         friendMapper.insert(friendCon);
 
-        //TODO:清对方缓存
-
+        //清对方缓存
+        clearCache(KEY+REQ+friendId);
 
         return new Result<>().ok("请求发送成功");
     }
@@ -76,32 +94,40 @@ public class FriendServiceImpl implements FriendService {
         if(friendCon!=null){
             friendCon.setStatus(1);
             friendMapper.updateById(friendCon);
-        }
-        //确认关系
-        FriendCon friendCon1=friendMapper.selectOne(Wrappers.<FriendCon>lambdaQuery().eq(FriendCon::getUserId, friendId).eq(FriendCon::getFriendId, userId));
-        if(friendCon1==null) {
-            friendCon1=new FriendCon();
-            friendCon1.setUserId(friendId);
-            friendCon1.setFriendId(userId);
-            friendCon1.setStatus(1);
-            friendMapper.insert(friendCon1);
-        }else{
-            friendCon1.setStatus(1);
-            friendMapper.updateById(friendCon1);
+            //确认关系
+            FriendCon friendCon1=friendMapper.selectOne(Wrappers.<FriendCon>lambdaQuery().eq(FriendCon::getUserId, friendId).eq(FriendCon::getFriendId, userId));
+            if(friendCon1==null) {
+                friendCon1=new FriendCon();
+                friendCon1.setUserId(friendId);
+                friendCon1.setFriendId(userId);
+                friendCon1.setStatus(1);
+                friendMapper.insert(friendCon1);
+            }else{
+                friendCon1.setStatus(1);
+                friendMapper.updateById(friendCon1);
+            }
         }
 
-        //TODO:清理双方缓存
-
+        //清理双方缓存
+        clearCache(KEY+REQ+friendId);
+        clearCache(KEY+REQ+userId);
+        clearCache(KEY+ACC+friendId);
+        clearCache(KEY+ACC+userId);
 
         return new Result<>().ok("添加成功");
     }
 
     @Override
     public Result friendDel(Integer friendId) {
+        Integer userId=ThreadLocalUtil.getUser().getId();
         FriendCon friendCon=new FriendCon();
         friendCon.setUserId(ThreadLocalUtil.getUser().getId());
         friendCon.setFriendId(friendId);
-        //TODO:清理双方缓存
+        //清理双方缓存
+        clearCache(KEY+REQ+friendId);
+        clearCache(KEY+REQ+userId);
+        clearCache(KEY+ACC+friendId);
+        clearCache(KEY+ACC+userId);
         friendMapper.deleteFriend(friendCon);
         return new Result<>().ok("删除成功");
     }
@@ -109,13 +135,36 @@ public class FriendServiceImpl implements FriendService {
     private List<User> getFriend(List<Integer> list)
     {
         List<User> res=new ArrayList<>();
-        for(Integer i:list)
-        {
-            User friend=userMapper.selectById(i);
-            friend.setPassword(null);
-            res.add(friend);
+        if(list!=null){
+            for(Integer i:list)
+            {
+                User friend=userMapper.selectById(i);
+                friend.setPassword(null);
+                res.add(friend);
+            }
         }
         return res;
+    }
+
+    private List<User> getCache(String key){
+        String str=stringRedisTemplate.opsForValue().get(key);
+        List<User> list = gson.fromJson(str, new TypeToken<List<User>>() {
+        }.getType());
+
+        if(list==null||list.isEmpty()) return null;
+
+        return list;
+    }
+
+    private void putCache(List<User> list,String key)
+    {
+        String json = gson.toJson(list);
+        stringRedisTemplate.opsForValue().set(key,json);
+    }
+
+    private void clearCache(String key)
+    {
+        stringRedisTemplate.opsForValue().getOperations().delete(key);
     }
 
 }
